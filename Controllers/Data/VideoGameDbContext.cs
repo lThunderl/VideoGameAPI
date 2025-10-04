a07a91fe20a00bcda7e3a96bb856848851c725be
lThunderl <111kyrie111@mail.ru>
2025-10-04 20:03:43 +0500

    Добавьте файлы проекта.

---
+++ Controllers/Data/VideoGameDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VideoGameAPI.Controllers.Data
+{
+    public class VideoGameDbContext(DbContextOptions<VideoGameDbContext> options) : DbContext(options)
+    {
+        public DbSet<VideoGame> VideoGames => Set<VideoGame>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VideoGame>().HasData(
+                new VideoGame
+                {
+                    Id = 1,
+                    Title = "The Legend of Zelda: Breath of the Wild",
+                    Platform = "Nintendo Switch",
+                    Developer = "Nintendo",
+                    Publisher = "Nintendo"
+                },
+                new VideoGame
+                {
+                    Id = 2,
+                    Title = "God of War",
+                    Platform = "PlayStation 4",
+                    Developer = "Santa Monica Studio",
+                    Publisher = "Sony Interactive Entertainment"
+                },
+                new VideoGame
+                {
+                    Id = 3,
+                    Title = "Red Dead Redemption 2",
+                    Platform = "PlayStation 4, Xbox One, PC",
+                    Developer = "Rockstar Games",
+                    Publisher = "Rockstar Games"
+                }
+            );
+        }
+    }
+}
---
+++ Controllers/VideoGameController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Xml;
+using VideoGameAPI.Controllers.Data;
+
+namespace VideoGameAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController(VideoGameDbContext context) : ControllerBase
+    {
+
+        private readonly VideoGameDbContext _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<List<VideoGame>>> GetVideoGames()
+        {
+            return Ok(await _context.VideoGames.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<VideoGame>> GetVideoGameById(int id)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+            if (game is null)
+            {
+                return NotFound();
+            }
+            return Ok(game);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<VideoGame>> AddVideoGame(VideoGame newGame)
+        {
+            if (newGame is null)
+            {
+                return BadRequest();
+            }
+
+            _context.VideoGames.Add(newGame);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame);
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> UpdateVideoGame(int id, VideoGame updateGame)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+
+            if (game is null)
+            {
+                return NotFound();
+            }
+
+            game.Title = updateGame.Title;
+            game.Platform = updateGame.Platform;
+            game.Publisher = updateGame.Publisher;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeleteVideoGame(int id)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+
+            if (game is null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(game);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
